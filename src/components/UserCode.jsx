70ff87f0f70cac55d31e2be80826ef47f91dab82
wissemknidif-ui <wissemknidif@gmail.com>
2026-03-14 10:11:12 +0100

    Create UserCode.jsx for user code display component

---
+++ src/components/UserCode.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserCode = () => {
+    return (
+        <div>
+            <h2>User Code Display</h2>
+            <pre>{`// User's code goes here`}</pre>
+        </div>
+    );
+};
+
+export default UserCode;
